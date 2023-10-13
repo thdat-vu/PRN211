5cfa0d51ad1662bdeccdcab0e69d28c9adb6b24f
Thanh Dat Vu <vuthanhdat181@gmail.com>
2023-10-13 10:01:14 +0700

    update delegate, extends lambda 13/10/2023

---
+++ Session03-OOPExt/Datvt.Delegate/Datvt.Delegate.InsideOut/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Datvt.Delegate.InsideOut
+{
+    //một cái tên đại diện cho 3 hàm, và còn nhiều hơn thế các hàm cùng signature
+    delegate void FDelegate(int x);
+    internal class Program
+    
+    {
+        /// <summary>
+        /// Nhận vào tham số x trả về x
+        /// </summary>
+        /// <param name="x"></param>
+        static void F1(int x)
+        {
+            Console.WriteLine("Call F1() with x = " + x);
+        }
+
+        /// <summary>
+        /// Nhận vào 1 tham số x và trả về x^2
+        /// </summary>
+        /// <param name="x"></param>
+        static void F2(int x)
+        {
+            Console.WriteLine("Call F2() with x^2 = " + Math.Pow(x, 2));
+        }
+        
+        /// <summary>
+        /// Nhận vào 1 tham số hàm là x và trả về x^3
+        /// </summary>
+        /// <param name="x"></param>
+        static void F3(int x)
+        {
+            Console.WriteLine("Call F3() with x^3 = " + Math.Pow(x, 3));
+        }
+        static void Main(string[] args)
+        {
+            FDelegate f = F1; // dấu() thì là gọi hàm, trong khi đây mới chỉ là trỏ tới hàm
+            f(42);
+            //f hay hơn biến thường, nó có thể trỏ cùng lúc nhiều hàm hơn bằng toán tử +=
+            f += F2;
+            f += F3;
+            //giờ ta gọi cùng lúc cả 2 hàm đi.
+            f(12);
+
+            //muốn f trỏ đến hàm F4(int x) trả về x^4, thì cách truyền thống, ta phải tạo hàm F4()
+            //riêng rồi gán f = F4()
+            //thì giờ tạo hàm ON-THE-GO, TAKE-AWAY. Đây là anonymous function
+            f += delegate (int x)
+            {
+                Console.WriteLine("Anonymous method F4() x^4: " + Math.Pow(x, 4));
+            };
+            Console.WriteLine("Invoke functions with x = 30");
+            f(30);
+            //nâng cấp tiếp tục anonymous method
+            //f = delegate (int x) { Console.WriteLine("anonymous method x^5: " + Math.Pow(x, 5)); };
+            //f = (int x) => { Console.WriteLine("anonymous method x^5: " + Math.Pow(x, 5)); };
+            f(2);
+            //vì sao chỗ này nó chỉ chạy mỗi hàm lambda mà ko chạy 4 hàm còn lại???
+            //xin thưa vì dùng 54 55 là phép gán ko phải phép cộng bằng
+            //sau dòng 54 và dòng 55 biến f từ giá trị F1() + F2() + F3() + F4() 
+            //trờ thành giá trị của f = anonymous expression ở trên.
+
+            ////LAMBDA EXPRESSION - Kỹ thuật rút gọn 1 hàm ẩn danh đến mức tối thiểu
+            f = x => { Console.WriteLine("Anonymous method  x^5 = " + Math.Pow(x, 5)); };
+            //bên C# cái lambda là => chứ ko phải là -> NHA NHA NHA
+            //triển thôi
+            f(54);
+        }
+    }
+}
---
+++ Session03-OOPExt/Datvt.Delegate/Datvt.Delegate.Lambda/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Threading.Channels;
+
+namespace Datvt.Delegate.Lambda
+{
+    /// <summary>
+    /// Hàm đại diện nhận 2 tham số đầu vào trả về 1 giá trị mà không quan tâm việc tính toán 
+    /// bên trong
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    delegate double TwoNumbersComputation(double x, double y);
+    internal class Program
+    {   
+        /// <summary>
+        /// Hàm tính toán nhận 2 tham số đầu vào là chiều rộng và chiều dài
+        /// trả về diện tích hình chữ nhật
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        static double ComputeRectangleArea(double width, double height) => width * height;
+        
+        /// <summary>
+        /// Hàm tính toán với 2 giá trị đầu vào là chiều dài và chiều rộng
+        /// trả về diện tích hình chữ nhật
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        static double CouputeRectanglePerimeter(double width, double height) => (width + height) * 2;
+        
+        
+        /// <summary>
+        /// hàm tính toán nhận 2 tham số là cạnh đáy và chiều cao
+        /// trả về diện tích hình tam giác
+        /// </summary>
+        /// <param name="baseEdge"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        static double TriangleArea(double baseEdge, double height) => baseEdge * height / 2; 
+        static void Main(string[] args)
+        {
+            //khai báo delegate
+            TwoNumbersComputation f = ComputeRectangleArea;
+            Console.WriteLine("Area of Rectangle: " +  f(20, 12));
+
+            //viết lại hàm ComputeRectangleArea bằng anonymous function:
+            f = delegate (double x , double y) { return (x + y) * 2; };
+            Console.WriteLine("The Perimeter of the Rectangle written by anonymous function: " + f(5, 6));
+            //viết lại hàm ComputeRectangleArea bằng lambda:
+            f = (x, y) => x * y /2 ;
+            Console.WriteLine("The Perimeter of the Triangle written by lambda expression: " + f(7,9));
+        }
+    }
+}
